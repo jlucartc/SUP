29c781671819bda1c661b75920af98527ca2ace8
Luca <Luca>
2017-02-22 17:50:28 -0300

    Commit inicial

---
+++ maisInfo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3">
+
+        </div>
+        <div class="col-sm-6">
+            <h2>Titulo do Plano</h2>
+            <h3>Escolha a configuração do seu plano: </h3>
+
+            <h3>Regras do plano:</h3>
+            <p>O plano possui um limite máximo de crédito e um limite minimo de pessoas. Caso o limite de pessoas não seja atingido, o plano não poderá ser confirmado no final do prazo. Da mesma forma, em caso de o limite de crédito ser ultrapassado, o plano não poderá ser confirmado no final do prazo. OBS: Mesmo que o limite de crédito não seja ultrapassado, se a quantidade mínima de contratantes não for atingida, o plano não poderá ser confirmado. No entanto, é importante lembrar que não há limite máximo de contratantes, desde que o limite de crédito seja respeitado.</p><br>
+
+            <form class="" action="index.html" method="post">
+              <div class="col-sm-6">
+              <input class="form-control" type="text" name="" value="" placeholder="Taxa de Juros"><br><label for="">
+              <input class="form-control" type="text" name="" value="" placeholder="Anuidade"><br>
+              <input class="form-control" type="text" name="" value="" placeholder="Crédito"><br>
+            </div>
+            </form>
+        </div>
+        <div class="col-sm3">
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ planos.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="color:#55aaff">Academicard</a>
+      <a class="navbar-brand" href="#"><img src="img/user.jpg" alt="" width="25" height="25"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Planos em andamento</a></li>
+      <li><a href="#">Planos contratados</a></li>
+    </ul>
+    <form class="navbar-form navbar-right" action="index.html" method="post">
+      <div class="input-group">
+        <input class="form-control" type="text" name="" value="" placeholder="Buscar">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="button" name="button"><span class="glyphicon glyphicon-search"></span></button>
+        </div>
+      </div>
+    </form>
+  </div>
+  </nav>
+  <div class="col-sm-3">
+    <div class="">
+    </div>
+  </div>
+  <div class="col-sm-6 " align="center">
+    <div class="container" style="margin:20px;padding:20px;background-color:#9999ff;width:80%;">
+      <h4>Plano 1 - Banco x</h4><br>
+        <h4>Credito disponível: R$ 55.000</h4>
+      <h5 class="alert alert-success" style="width:50%;">15/50 pessoas</h5><br>
+      <button class="btn btn-primary" type="button" name="button">Mais informações</button>
+    </div>
+    <div class="container" style="margin:20px;padding:20px;background-color:#99ff99;width:80%;">
+      <h4>Plano 1 - Banco y</h4><br>
+        <h4>Credito disponível: R$ 55.000</h4>
+      <h5 class="alert alert-warning" style="width:50%;">25/65 pessoas</h5><br>
+      <button class="btn btn-primary" type="button" name="button">Mais informações</button>
+    </div>
+    <div class="container" style="margin:20px;padding:20px;background-color:#ff9999;width:80%;">
+      <h4>Plano 1 - Banco z </h4><br>
+        <h4>Credito disponível: R$ 55.000</h4>
+      <h5 class="alert alert-danger" style="width:50%;">45/30 pessoas</h5><br>
+      <button class="btn btn-primary" type="button" name="button">Mais informações</button>
+    </div>
+  </div>
+  <div class="col-sm-3">
+  </div>
+</div>
+</body>
+</html>
